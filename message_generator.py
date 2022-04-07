93a730b96ec10e37647608620d611f73c7cd7715
BIG-Denis <102750547+BIG-Denis@users.noreply.github.com>
2022-04-07 17:22:06 +0300

    Add files via upload

--- message_generator.py
+++ message_generator.py
@@ -28,49 +28,56 @@ def write_source(raw_source: str):
 
 def generate_text() -> str:
 
+    # SliceGen functions
     def def_len_cut(_def_text: str) -> list:
         cuts = (None, None)
-        while cuts.count(None) == 2:
+        while cuts.count(None) == 2 or (cuts.count(None) == 0 and abs(cuts[0] - cuts[1]) < 3):
             cuts = choices([None, randint(1, len(_def_text))], weights=(10, 20), k=2)
         return sorted(cuts) if cuts.count(None) == 0 else cuts
 
+    def def_word_cut(_def_text: str) -> list:
+        words = _def_text.split(' ')
+        cuts = (None, None)
+        while cuts.count(None) == 2 or (cuts.count(None) == 0 and abs(cuts[0] - cuts[1]) < 2):
+            cuts = choices([None, randint(1, len(words))], weights=(10, 20), k=2)
+        return sorted(cuts) if cuts.count(None) == 0 else cuts
+
+    # TextGen functions
+    def def_rand_len(def_text: str, cuts: list) -> str:
+        return def_text[cuts[0]:cuts[1]]
+
+    def def_rand_word(def_text: str, cuts: list) -> str:
+        words = def_text.split(' ')
+        if len(words) > 5:
+            return f' {" ".join(words[cuts[0]:cuts[1]])} '
+        else:
+            return def_rand_len(def_text, def_len_cut(def_text))
+
+    def def_full(def_text: str) -> str:
+        if len(def_text) > 150:
+            return def_rand_word(def_text, def_word_cut(def_text))
+        else:
+            return def_text
+
+    # Final generating text function
+    def text_gen(_res_texts: list):
+        _text = ''
+        for text in _res_texts:
+            cut_type = (choices(['rand_len', 'rand_word', 'full'], weights=(50, 50, 50), k=1))[0]
+            if cut_type == 'full':
+                _text += def_full(text)
+            elif cut_type == 'rand_word':
+                _text += def_rand_word(text, def_word_cut(text))
+            elif cut_type == 'rand_len':
+                _text += def_rand_len(text, def_len_cut(text))
+        return _text
+
+    # Gen parameters
     generated_message: str = ''
     res_count = (choices([1, 2, 3, 4, 5], weights=(5, 35, 50, 35, 15), k=1))[0]
     res_texts = [choice(messages)[:-1] for _ in range(res_count)]
 
-    for text in res_texts:
-        cut = (None, None)
-        cut_type = (choices(['rand_len', 'rand_word', 'full'], weights=(50, 50, 50), k=1))[0]
-
-        def def_rand_len(def_text: str) -> None:
-            nonlocal cut, generated_message
-            cut = def_len_cut(def_text)
-            generated_message += def_text[cut[0]:cut[1]]
-
-        def def_rand_word(def_text: str) -> None:
-            nonlocal cut, generated_message
-            words = def_text.split(' ')
-            if len(words) > 5:
-                cut = def_len_cut(def_text)
-                generated_message += f' {" ".join(words[cut[0]:cut[1]])} '
-            else:
-                def_rand_len(def_text)
-
-        def def_full(def_text: str) -> None:
-            nonlocal generated_message
-            if len(def_text) > 150:
-                def_rand_word(def_text)
-            else:
-                generated_message += def_text
-
-        if cut_type == 'full':
-            def_full(text)
-        elif cut_type == 'rand_word':
-            def_rand_word(text)
-        elif cut_type == 'rand_len':
-            def_rand_len(text)
-
-    if len(generated_message) < 10:
-        generated_message = generate_text()
+    while len(generated_message) < 10:
+        generated_message = text_gen(res_texts)
 
     return generated_message
